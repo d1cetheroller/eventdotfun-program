80dc6bd8872c160ad291c82d9df56d4c8311cc6a
d1ceeth <erwinphanglius10@gmail.com>
2025-05-05 03:03:08 +0700

    add ErrorCodes

--- programs/eventdotfun-program/src/error.rs
+++ programs/eventdotfun-program/src/error.rs
@@ -2,6 +2,42 @@ use anchor_lang::prelude::*;
 
 #[error_code]
 pub enum ErrorCode {
-    #[msg("Custom error message")]
-    CustomError,
+    #[msg("Invalid Authority")]
+    InvalidAuthority,
+
+    #[msg("Invalid Sales Type, should be 1 or 2")]
+    InvalidSalesType,
+
+    #[msg("Invalid Timestamp")]
+    InvalidTimestamp,
+
+    #[msg("Invalid Exponent, should be in between 1 - 3")]
+    InvalidExponent,
+
+    #[msg("Invalid Price")]
+    InvalidPrice,
+
+    #[msg("Invalid Ticket Configuration")]
+    InvalidTicketConfiguration,
+
+    #[msg("Max Ticket Reached")]
+    MaxTicketReached,
+
+    #[msg("Curve Not Started Yet")]
+    CurveNotStarted,
+
+    #[msg("Curve Still On Progess")]
+    CurveStillOnProgress,
+
+    #[msg("Curve Ended")]
+    CurveEnded,
+
+    #[msg("Curve Reaches Threshold")]
+    CurveReachesThreshold,
+
+    #[msg("Curve Still Below Threshold")]
+    CurveStillBelowThreshold,
+
+    #[msg("Refund Not Opened")]
+    RefundNotOpened,
 }
